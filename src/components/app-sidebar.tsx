711d6a5ca93446c1858a6a1076cea48946b0fe58
Geuni <dlrmsgnl620@naver.com>
2025-03-11 00:47:19 +0900

    feat: routing

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,18 +1,66 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Link } from "@tanstack/react-router";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
-} from "@/components/ui/sidebar";
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "./ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "/inbox",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "/calendar",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "/search",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
--- src/main.tsx
+++ src/main.tsx
@@ -25,14 +25,68 @@ const indexRoute = createRoute({
   path: "/",
   component: function Index() {
     return (
-      <div className="bg-gray-300 w-full h-full flex items-center justify-center">
+      <div className="bg-gray-100 w-full h-full flex items-center justify-center">
         <h3 className="text-white text-2xl font-bold">Welcome Home!</h3>
       </div>
     );
   },
 });
 
-const routeTree = rootRoute.addChildren([indexRoute]);
+const inboxRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/inbox",
+  component: function Inbox() {
+    return (
+      <div className="bg-gray-200 w-full h-full flex items-center justify-center">
+        <h3 className="text-white text-2xl font-bold">Welcome Inbox!</h3>
+      </div>
+    );
+  },
+});
+
+const calendarRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/calendar",
+  component: function Calendar() {
+    return (
+      <div className="bg-gray-300 w-full h-full flex items-center justify-center">
+        <h3 className="text-white text-2xl font-bold">Welcome Calendar!</h3>
+      </div>
+    );
+  },
+});
+
+const searchRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/search",
+  component: function Search() {
+    return (
+      <div className="bg-gray-400 w-full h-full flex items-center justify-center">
+        <h3 className="text-white text-2xl font-bold">Welcome Search!</h3>
+      </div>
+    );
+  },
+});
+
+const settingsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/settings",
+  component: function Settings() {
+    return (
+      <div className="bg-gray-500 w-full h-full flex items-center justify-center">
+        <h3 className="text-white text-2xl font-bold">Welcome Settings!</h3>
+      </div>
+    );
+  },
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  inboxRoute,
+  calendarRoute,
+  searchRoute,
+  settingsRoute,
+]);
 
 const router = createRouter({ routeTree });
 
