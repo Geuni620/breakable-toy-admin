9424634729c3628ec92098c16373d3d8bbd7c69c
Geuni <dlrmsgnl620@naver.com>
2025-04-04 01:28:32 +0900

    chore: ignore

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,7 @@ import "../src/index.css";
 const preview: Preview = {
   parameters: {
     controls: {
+      disableSaveFromUI: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
