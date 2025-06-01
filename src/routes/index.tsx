bbbd15cc50f4de65f0435c3aeb2e5d75d2ce2999
Geuni <dlrmsgnl620@naver.com>
2025-06-01 20:28:30 +0900

    feat: data-table

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,38 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "@/components/data-table";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
+interface Payment {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+];
+
 function RouteComponent() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <h1 className="text-2xl font-bold">Home</h1>
+      <DataTable data={[]} columns={columns} />
     </div>
   );
 }
